895aad765605f7eea6d0bc4e139453f5d476d25d
Emre Can <yeleremre@hotmail.com>
2022-04-22 23:49:08 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      flex: {
+        '0': '0 0 auto'
+      },
+      spacing: {
+        '18': '5rem',
+        'left-f': '35.55%',
+        'left-p': '30%',
+        'right-p': '10%',
+        'basket': '6rem',
+      }
+    },
+  },
+  plugins: [],
+}
